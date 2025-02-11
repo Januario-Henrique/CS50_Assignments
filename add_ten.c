bececcab20e3f0cbdca82c75f531e2ea4debf8eb
Januario-Henrique <januarioinocenciohenriques@gmail.com>
2025-02-11 23:08:10 +0200

    Create add_ten.c
    add ten to any number given

---
+++ add_ten.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int num, result;
+    printf("Enter a number:\n");
+    scanf("%d", &num);
+    result = num + 10;
+    printf("The result is: %d/n", result);
+    return 0;
+}
